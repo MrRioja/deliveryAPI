8825a039775aea648b48b17671cd6ddbd88c6cd6
Luiz Rioja <lulyrioja@gmail.com>
2022-01-23 23:59:50 -0300

    :alien: Importing route files and creating error handling middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Salve" });
+app.use(express.json());
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
 });
 
 app.listen(process.env.PORT_APP, () =>
