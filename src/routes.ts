a8a291fdfac6560e4334535a70abf4104fd34d67
Luiz Rioja <lulyrioja@gmail.com>
2022-01-24 23:36:25 -0300

    :alien: Creating delivery creation route

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
@@ -20,5 +22,7 @@ routes.post(
 
 routes.post("/client", createClientController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle);
+
+routes.post("/delivery", createDeliveryController.handle);
 
 export { routes };
